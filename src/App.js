bf628456ab3df02be199ed60493278ade60134ad
danaOUSL <dananjaya6005@gmail.com>
2023-07-26 12:31:05 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gift of hopes</h1>
+    </div>
+  );
+}
+
+export default App;
